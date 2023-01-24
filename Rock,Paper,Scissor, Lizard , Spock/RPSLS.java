5ab04f4c0fbfbdc1c97bb66aff246d1061ef90e8
christopherferraina <83135510+christopherferraina@users.noreply.github.com>
2023-01-24 11:31:39 -0500

    Add files via upload

---
+++ Rock,Paper,Scissor, Lizard , Spock/RPSLS.java
@@ -0,0 +1,8 @@
+public class RPSLS
+{
+  public static void main (String[] args)
+  {
+    RPSLS_Game rs = new RPSLS_Game();
+    rs.start();
+  }
+}
---
+++ hangman/DrawHangman.java
@@ -0,0 +1,178 @@
+public class DrawHangman
+{
+  
+  public static void main (String[] args) 
+  {
+    
+    
+    
+    
+    
+    
+  } 
+  
+  
+  
+  
+  public void missedNone()
+  {
+    
+    
+    System.out.println("    --------");
+    System.out.println("    |      |");   
+    System.out.println("           |");
+    System.out.println("           |");
+    System.out.println("           |");
+    System.out.println("           |");
+    System.out.println("           |");
+    System.out.println("           |");
+    System.out.println("           |");
+    System.out.println("           |");
+    System.out.println("           |");
+    System.out.println("           |");
+    System.out.println("           |");
+    
+    
+    
+    
+    
+    
+    
+  }
+  
+  
+  public void missone()
+  {
+    
+    System.out.println("    --------");
+    System.out.println("    |      |");   
+    System.out.println("   ---     |");
+    System.out.println("  |   |    |");
+    System.out.println("   ---     |");
+    System.out.println("           |");
+    System.out.println("           |");
+    System.out.println("           |");
+    System.out.println("           |");
+    System.out.println("           |");
+    System.out.println("           |");
+    System.out.println("           |");
+    System.out.println("           |");
+    
+    
+  }
+  public void misstwo()
+  {
+  
+  
+        System.out.println("    --------");
+    System.out.println("    |      |");   
+    System.out.println("   ---     |");
+    System.out.println("  |   |    |");
+    System.out.println("   ---     |");
+    System.out.println("    |      |");
+    System.out.println("    |      |");
+    System.out.println("    |      |");
+    System.out.println("    |      |");
+    System.out.println("    |      |");
+    System.out.println("           |");
+    System.out.println("           |");
+    System.out.println("           |");
+    
+  
+  
+  
+  
+  
+  }
+  
+  public void missthree()
+  {
+    
+           System.out.println("    --------");
+    System.out.println("    |      |");   
+    System.out.println("   ---     |");
+    System.out.println("  |   |    |");
+    System.out.println("   ---     |");
+    System.out.println("    |      |");
+    System.out.println("    |      |");
+    System.out.println("----|      |");
+    System.out.println("    |      |");
+    System.out.println("    |      |");
+    System.out.println("           |");
+    System.out.println("           |");
+    System.out.println("           |"); 
+    
+    
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+  }
+public void missedfour()
+{
+         System.out.println("    --------");
+    System.out.println("    |      |");   
+    System.out.println("   ---     |");
+    System.out.println("  |   |    |");
+    System.out.println("   ---     |");
+    System.out.println("    |      |");
+    System.out.println("    |      |");
+    System.out.println("----|----  |");
+    System.out.println("    |      |");
+    System.out.println("    |      |");
+    System.out.println("           |");
+    System.out.println("           |");
+    System.out.println("           |"); 
+
+
+
+}
+public void missedfive()
+  {
+         System.out.println("    --------");
+    System.out.println("    |      |");   
+    System.out.println("   ---     |");
+    System.out.println("  |   |    |");
+    System.out.println("   ---     |");
+    System.out.println("    |      |");
+    System.out.println("    |      |");
+    System.out.println("----|----  |");
+    System.out.println("    |      |");
+    System.out.println("    |      |");
+    System.out.println("   /       |");
+    System.out.println("  /        |");
+    System.out.println(" /         |"); 
+
+
+
+}
+public void missedsix()
+    {
+         System.out.println("    --------");
+    System.out.println("    |      |");   
+    System.out.println("   ---     |");
+    System.out.println("  |   |    |");
+    System.out.println("   ---     |");
+    System.out.println("    |      |");
+    System.out.println("    |      |");
+    System.out.println("----|----  |");
+    System.out.println("    |      |");
+    System.out.println("    |      |");
+    System.out.println("   / |     |");
+    System.out.println("  /   |    |");
+    System.out.println(" /     |   |"); 
+
+
+
+}
+
+}
+
+
